071cabb8d0a73c97423649d2dfe15d8f5caef49d
Farmstead001 <131138226+Farmstead001@users.noreply.github.com>
2023-11-06 20:13:25 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,92 @@
-let counter = 0
+let counter1 = 0
+let counter2 = 0
+let counter3 = 0
+let counter4 = 0
 
-function subtract() {
-    counter--;
+function subtract(counter) {
+    switch (counter) {
+        case 1:
+            counter1--;
+            document.getElementById("counter1").innerHTML = counter1;
+            break;
 
-    console.log(counter);
+        case 2:
+            counter2--;
+            document.getElementById("counter2").innerHTML = counter2;
+            break;
+
+        case 3:
+            counter3--;
+            document.getElementById("counter3").innerHTML = counter3;
+            break;
+
+        case 4:
+            counter4--;
+            document.getElementById("counter4").innerHTML = counter4;
+            break;
+    }
+
+    console.log(id, counter, counter1, counter2, counter3, counter4);
 };
 
-function add() {
-    counter++;
+function add(counter) {
+    switch (counter) {
+        case 1:
+            counter1++;
+            document.getElementById("counter1").innerHTML = counter1;
+            break;
 
-    console.log(counter);
+        case 2:
+            counter2++;
+            document.getElementById("counter2").innerHTML = counter2;
+            break;
+
+        case 3:
+            counter3++;
+            document.getElementById("counter3").innerHTML = counter3;
+            break;
+
+        case 4:
+            counter4++;
+            document.getElementById("counter4").innerHTML = counter4;
+            break;
+    }
+
+    console.log(id, counter, counter1, counter2, counter3, counter4);
 };
+
+function save() {
+    localStorage.setItem("counter1", counter1);
+    localStorage.setItem("counter2", counter2);
+    localStorage.setItem("counter3", counter3);
+    localStorage.setItem("counter4", counter4);
+}
+
+function load() {
+    counter1 = localStorage.getItem("counter1");
+    counter2 = localStorage.getItem("counter2");
+    counter3 = localStorage.getItem("counter3");
+    counter4 = localStorage.getItem("counter4");
+
+    document.getElementById("counter1").innerHTML = counter1;
+    document.getElementById("counter2").innerHTML = counter2;
+    document.getElementById("counter3").innerHTML = counter3;
+    document.getElementById("counter4").innerHTML = counter4;
+}
+
+function reset() {
+    counter1 = 0;
+    counter2 = 0;
+    counter3 = 0;
+    counter4 = 0;
+
+    localStorage.setItem("counter1", counter1);
+    localStorage.setItem("counter2", counter2);
+    localStorage.setItem("counter3", counter3);
+    localStorage.setItem("counter4", counter4);
+
+    document.getElementById("counter1").innerHTML = counter1;
+    document.getElementById("counter2").innerHTML = counter2;
+    document.getElementById("counter3").innerHTML = counter3;
+    document.getElementById("counter4").innerHTML = counter4;
+}
